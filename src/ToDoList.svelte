365720677546491ed512492dc99e13dd1ea5fb7b
mbp <sunggi1211@icloud.com>
2022-01-03 16:06:49 +0900

    TODO tutorial

---
+++ src/ToDoList.svelte
@@ -0,0 +1,32 @@
+<script>
+    import { toDoItems } from "./store.js";
+    import { fade, scale } from "svelte/transition";
+
+    function removeFromList(i) {
+        $toDoItems.splice(i, 1);
+        $toDoItems = $toDoItems;
+    }
+</script>
+
+{#each $toDoItems as item, index}
+    <div class="toDoItems" in:scale out:fade="{{ dration: 500 }}">
+        <input bind:checked={item.status} type="checkbox" id="checked_{index}">
+        <label for="checked_{index}">
+            <span class:checked={item.status}>{item.text}</span>
+        </label>
+        <button on:click={() => removeFromList(index)}>削除</button>
+        <br/>
+    </div>
+{/each}
+
+<style>
+    .checked {
+        text-decoration: line-through;
+    }
+    .toDoItems {
+        text-align: left;
+    }
+    label {
+        display: inline;
+    }
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import App from './App.svelte';
+
+const app = new App({
+	// bodyタグに出力される
+	target: document.body,
+	//props: {
+		//name: 'world'
+	//}
+});
+
+export default app;
